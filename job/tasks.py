e823e43607e746d82d29a43cdcec3292e92b571f
root <admin@example.com>
2020-03-10 16:29:32 +0800

    playbook执行增加参数

--- job/tasks.py
+++ job/tasks.py
@@ -3,15 +3,19 @@ from celery import shared_task
 from common.ansible_api import PlayBook
 from multiprocessing import current_process
 import logging
+import traceback
 
 logger = logging.getLogger("default")
 
 @shared_task
 def run_playbook(host_list,playbook,extra_vars=None):
     current_process()._config = {'semprefix': '/mp'}
-    playbook_obj = PlayBook(host_list,run_playbook.request.id,extra_vars=extra_vars)
-    res = playbook_obj.runPlayBook(playbook)
-    return res
+    try:
+        playbook_obj = PlayBook(host_list,run_playbook.request.id,extra_vars=extra_vars)
+        res = playbook_obj.runPlayBook(playbook)
+        return res
+    except Exception:
+        logger.error("[ERROR] " + traceback.format_exc())
 
 @shared_task
 def add(a,b):
